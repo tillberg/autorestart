851b1929c69ca229da458db582611bbadf14db4c
Dan Tillberg <dan@tillberg.us>
2018-03-26 18:51:49 -0600

    Add support for polling

--- autorestart.go
+++ autorestart.go
@@ -9,7 +9,7 @@ import (
 	"syscall"
 	"time"
 
-	"gopkg.in/fsnotify.v1"
+	"github.com/tillberg/watcher"
 )
 
 func logf(format string, args ...interface{}) {
@@ -44,7 +44,7 @@ func getExePath() string {
 // This function is a wrapper around NotifyOnChange and RestartViaExec, calling the
 // latter when the former signals that a change was detected.
 func RestartOnChange() {
-	notifyChan := NotifyOnChange()
+	notifyChan := NotifyOnChange(true)
 	<-notifyChan
 	logf("%s changed. Restarting via exec.", getExePath())
 	// Sort of a maybe-workaround for the issue detailed in RestartViaExec:
@@ -55,35 +55,20 @@ func RestartOnChange() {
 // Subscribe to a notification when the current process' executable file is modified.
 // Returns a channel to which notifications (just `true`) will be sent whenever a
 // change is detected.
-func NotifyOnChange() chan bool {
+func NotifyOnChange(usePolling bool) chan bool {
 	notifyChan := make(chan bool)
 	go func() {
 		exePath := getExePath()
 		if exePath == errorPath {
 			return
 		}
-		exeDir := filepath.Dir(exePath)
-		watcher, err := fsnotify.NewWatcher()
+		notify, err := watcher.WatchExecutable(exePath, usePolling)
 		if err != nil {
-			logf("Failed to initialize gopkg.in/fsnotify.v1 watcher: %s", err)
+			logf("Failed to initialize watcher: %v", err)
 			return
 		}
-		abs, _ := filepath.Abs(exeDir)
-		err = watcher.Add(abs)
-		if err != nil {
-			logf("Failed to start filesystem watcher on %s: %s", exeDir, err)
-			return
-		}
-		for {
-			select {
-			case err := <-watcher.Errors:
-				logf("Watcher error: %s", err)
-			case ev := <-watcher.Events:
-				// log.Println("change", ev.Name, exePath, ev)
-				if ev.Name == exePath {
-					notifyChan <- true
-				}
-			}
+		for range notify {
+			notifyChan <- true
 		}
 	}()
 	return notifyChan
@@ -99,10 +84,7 @@ func RestartViaExec() {
 		return
 	}
 	for {
-		args := os.Args
-		env := os.Environ()
-		// logf("calling syscall.Exec with %q, %q, %q", exePath, args, env)
-		err := syscall.Exec(exePath, args, env)
+		err := syscall.Exec(exePath, os.Args, os.Environ())
 		// Not sure if this is due to user error, a Go regression in 1.5.x, or arch something,
 		// but this started failing when called immediately; a short delay (perhaps to switch
 		// to a different thread? or maybe to actually delay for some reason?) seems to work
